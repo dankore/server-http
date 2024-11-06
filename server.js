a88d791f6a2b049e4e9bb301c93ff4ee819ab036
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2024-11-06 10:41:47 -0600

    Refactor

--- server.js
+++ server.js
@@ -1,44 +1,63 @@
 const http = require('node:http');
-const fs = require('node:fs')
+const fs = require('node:fs');
+const path = require('node:path');
 
 const server = http.createServer();
 const PORT = 8000;
 
 server.on('request', (request, response) => {
-  // PATH
-  fs.readFile('./index.html', (error, content) => {
-    if (request.url === '/') {
-      // send index.html
-      if (error) {
-        // yes error, send error message to the client/browser
-        // HTTP Status
-        response.writeHead(500, { 'Content-Type': 'text/plain' });
-        response.end('Server da samu error');
-      } else {
-        // no error, send HTML file to the client/browser
+  switch (request.url) {
+    case '/': {
+      fs.readFile(path.join(__dirname, 'index.html'), (error, content) => {
+        if (error) {
+          response.writeHead(500, { 'Content-Type': 'text/plain' });
+          response.end('Server da samu error');
+          return;
+        }
+
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.end(content);
-      }
-    }
+      });
 
-    if (request.url == '/styles.css') {
-      // send css
-      fs.readFile('./styles.css', (error, content) => {
-        if (error) {
-          response.writeHead(500, { 'Content-Type': 'text/plain'})
-          response.end('Server error')
-        } else {
-          response.writeHead(200, {'Content-Type': 'text/css'})
-          response.end(content)
-        }
-      })
+      break;
     }
-  
-  })
-})
+    case '/styles.css': {
+      fs.readFile(path.join(__dirname, 'styles.css'), (error, content) => {
+        if (error) {
+          response.writeHead(500, { 'Content-Type': 'text/plain' });
+          response.end('Server da samu error');
+          return;
+        }
+
+        response.writeHead(200, { 'Content-Type': 'text/css' });
+        response.end(content);
+      });
+
+      break;
+    }
+    case '/script.js': {
+      fs.readFile(path.join(__dirname, 'script.js'), (error, content) => {
+        if (error) {
+          response.writeHead(500, { 'Content-Type': 'text/plain' });
+          response.end('Server da samu error');
+          return;
+        }
+
+        response.writeHead(200, { 'Content-Type': 'text/javascript' });
+        response.end(content);
+      });
+
+      break;
+    }
+    default: {
+      response.writeHead(404, { 'Content-Type': 'text/plain' });
+      response.end('Not found');
+    }
+  }
+});
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`)
+  console.log(`Server is running on port: ${PORT}`);
 });
 
 // DRY principle: DON'T REPEAT YOURSELF
